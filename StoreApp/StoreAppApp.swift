fbdd96858f2b65c8865a9457aee5b7eed423044e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-10-13 10:02:41 +0300

    Initial Commit

---
+++ StoreApp/StoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAppApp.swift
+//  StoreApp
+//
+//  Created by ramil on 13.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct StoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
